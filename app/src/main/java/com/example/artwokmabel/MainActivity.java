ae733565dc36bc96695d15efbd522743619fe896
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-09-16 20:02:44 +0800

    Firebase setup
    Firebase setup

--- app/src/main/java/com/example/artwokmabel/MainActivity.java
+++ app/src/main/java/com/example/artwokmabel/MainActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+//    }
 }
