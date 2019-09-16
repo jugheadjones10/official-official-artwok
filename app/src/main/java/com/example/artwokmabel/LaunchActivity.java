21334e4d90ed66bdb0517e542d23dc1842a32b9f
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-09-16 23:10:33 +0800

    Android working? fb
    fk

--- app/src/main/java/com/example/artwokmabel/LaunchActivity.java
+++ app/src/main/java/com/example/artwokmabel/LaunchActivity.java
@@ -1,5 +1,5 @@
 package com.example.artwokmabel;
 
-public class launchActivity {
+public class LaunchActivity {
 
 }
--- app/src/main/java/com/example/artwokmabel/MainActivity.java
+++ app/src/main/java/com/example/artwokmabel/MainActivity.java
@@ -1,18 +1,109 @@
 package com.example.artwokmabel;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 //import android.support.constraint.ConstraintLayout;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_page);
+
+        loginButton = findViewById(R.id.facebook_button);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if(currentAccessToken == null){
+//                txtName.setText("");
+//                txtEmail.setText("");
+                Toast.makeText(MainActivity.this, "User Logged out", Toast.LENGTH_LONG).show();
+            }else{
+                loadUserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+//                    txtEmail.setText(email);
+//                    txtName.setText(first_name +" "+ last_name);
+                    RequestOptions requestOptions = new RequestOptions();
+                    requestOptions.dontAnimate();
+
+//                    Glide.with(MainActivity.this).load(image_url).into(circleImageView);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name,last_name,email,id,");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
 }
