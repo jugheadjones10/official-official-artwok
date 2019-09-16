5e0a06906aae4f63ee5b73a8ee9c7af08e625270
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-09-16 19:55:36 +0800

    Colors
    Global color vars

---
+++ app/src/main/java/global_vars.java
@@ -0,0 +1,9 @@
+public class global_vars {
+
+    public String red = "ed2423";
+    public String orange = "f5a525";
+    public String blue = "29358f";
+    public String black = "353839";
+    public String backgroundFrom = "FF0467";
+    public String backgroundTo = "FC7437";
+}
