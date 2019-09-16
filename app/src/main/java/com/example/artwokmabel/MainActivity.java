2b923384846e78e21c78c5f02889356969af045b
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-09-16 21:32:32 +0800

    New broken fb
    plz

--- app/src/main/java/com/example/artwokmabel/MainActivity.java
+++ app/src/main/java/com/example/artwokmabel/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.artwokmabel;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import android.support.constraint.ConstraintLayout;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        mAuth = FirebaseAuth.getInstance();
+
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.launch_page);
     }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        updateUI(currentUser);
-//    }
 }
--- app/src/main/java/com/example/artwokmabel/launchActivity.java
+++ app/src/main/java/com/example/artwokmabel/launchActivity.java
@@ -1,4 +1,5 @@
 package com.example.artwokmabel;
 
 public class launchActivity {
+
 }
