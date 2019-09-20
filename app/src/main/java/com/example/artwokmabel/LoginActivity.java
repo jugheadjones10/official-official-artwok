bf07cd290532fee2013b97a43c92f11c8c8f7395
Moon Geon Sik <kunsik11@gmail.com>
2019-09-20 15:36:21 +0800

    Added ForgotPassword feature + Linked ForgotPassword button

--- app/src/main/java/com/example/artwokmabel/LoginActivity.java
+++ app/src/main/java/com/example/artwokmabel/LoginActivity.java
@@ -1,4 +1,28 @@
 package com.example.artwokmabel;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView forgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        forgotPassword = findViewById(R.id.tvforgotpass);
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
+            }
+        });
+    }
 }
