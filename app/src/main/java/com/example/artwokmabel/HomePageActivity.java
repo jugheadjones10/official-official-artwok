9711ad31d5b6f7308e401ebf523b7392e37dcb28
Ng Zhao Ming <ngzhaoming@gmail.com>
2019-09-21 17:50:13 +0800

    add navigation functions for buttons
    Navigation functions added up till the upload post page.
    Also added in the option to create a new account.

--- app/src/main/java/com/example/artwokmabel/HomePageActivity.java
+++ app/src/main/java/com/example/artwokmabel/HomePageActivity.java
@@ -2,14 +2,31 @@ package com.example.artwokmabel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    private ImageButton profile_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        profile_button = findViewById(R.id.profile_button);
+
+
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), ProfilePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/artwokmabel/ProfilePageActivity.java
+++ app/src/main/java/com/example/artwokmabel/ProfilePageActivity.java
@@ -2,7 +2,9 @@ package com.example.artwokmabel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ProfilePageActivity extends AppCompatActivity {
@@ -17,5 +19,13 @@ public class ProfilePageActivity extends AppCompatActivity {
         upload_post = findViewById(R.id.upload_button);
 
 
+        upload_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), UploadPostActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
