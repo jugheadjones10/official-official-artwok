6b910dad0b23b3b51276626abe8375f600ba52bc
Ng Zhao Ming <ngzhaoming@gmail.com>
2019-09-21 17:45:29 +0800

    add navigation functions for buttons
    Navigation functions added up till the upload post page.
    Also added in the option to create a new account.

--- app/src/main/java/com/example/artwokmabel/ProfilePageActivity.java
+++ app/src/main/java/com/example/artwokmabel/ProfilePageActivity.java
@@ -3,12 +3,19 @@ package com.example.artwokmabel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ProfilePageActivity extends AppCompatActivity {
+
+    private Button upload_post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        upload_post = findViewById(R.id.upload_button);
+
+
     }
 }
