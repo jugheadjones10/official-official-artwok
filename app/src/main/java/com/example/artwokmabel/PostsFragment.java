732b35a7a8ec61359fcbb34ccb7c46907887e6c9
Ng Zhao Ming <ngzhaoming@gmail.com>
2019-09-23 23:38:50 +0800

    beautify fragment and add upload navigation

--- app/src/main/java/com/example/artwokmabel/PostsFragment.java
+++ app/src/main/java/com/example/artwokmabel/PostsFragment.java
@@ -1,15 +1,20 @@
 package com.example.artwokmabel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 public class PostsFragment extends Fragment {
     View view;
+    Button upload_button;
 
     public PostsFragment() {
 
@@ -19,6 +24,18 @@ public class PostsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_posts_fragment, container, false);
+        Button button = (Button) view.findViewById(R.id.profile_upload);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getApplicationContext(), UploadPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 
