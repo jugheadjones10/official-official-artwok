51ff8b9c5ffd7f71a16078fc125001108a126ac8
Kim Young Jin <kimyoungjin1001@gmail.com>
2020-01-18 22:14:07 +0800

    Inside listings menu  (report for others, edit/delete for me)

--- app/src/main/java/com/example/artwokmabel/homepage/activities/IndivListingActivity.java
+++ app/src/main/java/com/example/artwokmabel/homepage/activities/IndivListingActivity.java
@@ -1,6 +1,9 @@
 package com.example.artwokmabel.homepage.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +16,7 @@ import com.example.artwokmabel.homepage.adapters.IndivListViewPagerAdapter;
 import com.example.artwokmabel.homepage.models.Listing;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.ImageListener;
 
@@ -21,11 +25,14 @@ import java.util.ArrayList;
 public class IndivListingActivity extends AppCompatActivity {
 
     private ActivityIndivListingNewNewBinding binding;
+    private FirebaseAuth mAuth;
+    private Listing listing;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mAuth = FirebaseAuth.getInstance();
         binding = DataBindingUtil.setContentView(this, R.layout.activity_indiv_listing_new_new);
 
         IndivListViewPagerAdapter adapter = new IndivListViewPagerAdapter(this);
@@ -54,8 +61,8 @@ public class IndivListingActivity extends AppCompatActivity {
 
     private void getIncomingIntent(){
 
-        Listing listing = (Listing)getIntent().getSerializableExtra("listing");
-
+        listing = (Listing)getIntent().getSerializableExtra("listing");
+        binding.setListing(listing);
 
         ArrayList<String> images = listing.getPhotos();
         binding.listingImage.setImageListener(new ImageListener() {
@@ -68,5 +75,47 @@ public class IndivListingActivity extends AppCompatActivity {
                         .into(imageView);
             }
         });
+
+        //Load profile pic into listing
+//        Picasso.get()
+//                .load(listing.get)
+//                .placeholder(R.drawable.user)
+//                .error(R.drawable.rick_and_morty)
+//                .into(imageView);
+
+        setSupportActionBar(binding.indivToolbar);
+        if(mAuth.getCurrentUser().getUid().equals(listing.getUserid())){
+            binding.indivToolbar.inflateMenu(R.menu.indiv_listing_menu_mine);
+        }else{
+            binding.indivToolbar.inflateMenu(R.menu.indiv_listing_menu_yours);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        if(mAuth.getCurrentUser().getUid().equals(listing.getUserid())){
+            inflater.inflate(R.menu.indiv_listing_menu_mine, menu);
+        }else{
+            inflater.inflate(R.menu.indiv_listing_menu_yours, menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.listing_delete:
+                return true;
+            case R.id.listing_edit:
+                return true;
+            case R.id.listing_report:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
