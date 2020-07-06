a762c3240df25a5341b96df224d9d14ee3957a95
Kim Young Jin <kimyoungjin1001@gmail.com>
2020-07-07 00:11:04 +0800

    Video

--- app/src/main/java/com/example/artwokmabel/profile/uploadlisting/UploadListingDescViewModel.java
+++ app/src/main/java/com/example/artwokmabel/profile/uploadlisting/UploadListingDescViewModel.java
@@ -1,4 +1,37 @@
 package com.example.artwokmabel.profile.uploadlisting;
 
-class UploadListingDescViewModel extends ViewModel implements UploadViewModel {
+import android.net.Uri;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.artwokmabel.UploadViewModel;
+
+public class UploadListingDescViewModel extends ViewModel implements UploadViewModel {
+
+    private MutableLiveData<Uri> imagePath = new MutableLiveData<>();
+    private MutableLiveData<Uri> videoPath = new MutableLiveData<>();
+
+    public UploadListingDescViewModel(){
+
+    }
+
+    public void setResultOk(Uri imagePath){
+        this.imagePath.setValue(imagePath);
+    }
+    public void setVideoResultOk(Uri videoPath){
+        this.videoPath.setValue(videoPath);
+    }
+
+
+    public LiveData<Uri> getImagePath() {
+        return imagePath;
+    }
+
+    public LiveData<Uri> getVideoPath() {
+        return videoPath;
+    }
+
+
 }
--- app/src/main/java/com/example/artwokmabel/profile/uploadlisting/UploadListingViewModel.java
+++ app/src/main/java/com/example/artwokmabel/profile/uploadlisting/UploadListingViewModel.java
@@ -1,4 +1,56 @@
 package com.example.artwokmabel.profile.uploadlisting;
 
-class UploadListingViewModel {
+import android.net.Uri;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.artwokmabel.UploadViewModel;
+import com.example.artwokmabel.repos.FirestoreRepo;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class UploadListingViewModel extends ViewModel implements UploadViewModel {
+
+    private MutableLiveData<Uri> imagePath = new MutableLiveData<>();
+    private MutableLiveData<Uri> videoPath = new MutableLiveData<>();
+    private MutableLiveData<Boolean> uploadSuccess = new MutableLiveData<>();
+
+    public UploadListingViewModel(){
+
+    }
+
+    public MutableLiveData<Boolean> getUploadSuccess() {
+        return uploadSuccess;
+    }
+
+    public void setResultOk(Uri imagePath){
+        this.imagePath.setValue(imagePath);
+    }
+    public void setVideoResultOk(Uri videoPath){
+        this.videoPath.setValue(videoPath);
+    }
+
+    public LiveData<Uri> getImagePath() {
+        return imagePath;
+    }
+    public LiveData<Uri> getVideoPath() {
+        return videoPath;
+    }
+
+    public void uploadNewListing(String postTitle, String postDesc, ArrayList<String> categories, Long price, String delivery, String refund, String currentUserId, ArrayList<String> postImageUris){
+        FirestoreRepo.getInstance().uploadNewListing(postTitle, postDesc, categories, price, delivery, refund, currentUserId, postImageUris, (success) -> {
+            if(success){
+                uploadSuccess.setValue(true);
+            }else{
+                uploadSuccess.setValue(false);
+            }
+        });
+    }
+
+    public interface UploadListingCallback{
+        void uploadListingCallback(boolean successful);
+    }
 }
