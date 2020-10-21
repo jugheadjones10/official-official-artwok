00657997020b03bfd2f019aa21013c083e673274
Kim Young Jin <kimyoungjin1001@gmail.com>
2020-10-21 23:07:32 +0900

    New official user

--- app/src/main/java/com/example/artwokmabel/repos/FirestoreRepo.java
+++ app/src/main/java/com/example/artwokmabel/repos/FirestoreRepo.java
@@ -71,7 +71,7 @@ public class FirestoreRepo {
     private final String TAG = "FirestoreRepo";
     private FirebaseFirestore db;
     private static FirestoreRepo firestoreRepo;
-    public final static String artwokId = "79XPyNUe0bTvluv8NywZMe9c5Vl1";
+    public final static String artwokId = "XqgxiUy5PkToQXFexnhbMWjE4SX2";
 
     public FirestoreRepo() {
         db = FirebaseFirestore.getInstance();
